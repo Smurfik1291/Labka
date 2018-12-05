8f2cc45ca3575cb3c31533841a20921557d60ce4
Smurfik1291 <45573685+Smurfik1291@users.noreply.github.com>
2018-12-05 12:42:07 +0200

    laba2

---
+++ laba2.1.js
@@ -0,0 +1,29 @@
+var name ="Kikavets Anatoliy";
+
+alert(name);
+
+var vari ="Variant 5";
+
+alert(vari);
+
+
+
+//task1
+
+function cube(numb){
+
+	return(numb*numb*numb)
+
+}
+
+
+
+alert("Task 1")
+
+var numb;
+
+numb=prompt("Enter number of your variant: ");
+
+
+
+alert("Your variant in cube: " +cube(numb));
