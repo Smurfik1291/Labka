ccdbe07872ef0e87de9e87c732dedcd6af26bb82
Smurfik1291 <45573685+Smurfik1291@users.noreply.github.com>
2018-12-05 13:04:10 +0200

    Add files via upload

---
+++ laba6.js
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <title>6 lab</title>
+
+    <link rel="stylesheet" href="css.css">
+
+  </head>
+
+ class Bookmarks {
+
+    constructor(id, name, description, link, IdExist) {
+
+        this.id = id;
+
+        this.name = name;
+
+        this.description = description;
+
+        this.link = link;
+
+        this.IdExist = IdExist;
+
+
+
+        this.str = "<li id='" + this.id + "'><a href='" + this.link + "'><b>" + this.name + ": </b>" + this.description + "</a></li>";
+
+        if (IdExist == false) {
+
+          $("#ForBookmarks").html($("#ForBookmarks").html()+this.str);
+
+        }
+
+        else {
+
+          $("#"+id).html("<a href='" + this.link + "'><b>" + this.name + ": </b>" + this.description + "</a>");
+
+        }
+
+    }
+
+}
+
+
+
+var ArrayOfID = [];
+
+
+
+function AddLisner () {
+
+  var id = prompt("Id:");
+
+  id = parseInt(id);
+
+  if (id == ArrayOfID[id-1]){
+
+    let bookmark = new Bookmarks (id,prompt("Name:"),prompt("Description:"),prompt("Link:"), true);
+
+  }
+
+  else {
+
+    let bookmark = new Bookmarks (id,prompt("Name:"),prompt("Description:"),prompt("Link:"), false);
+
+    ArrayOfID[id-1] = id;
+
+  }
+
+}
+
+
+
+function DeleteLisner () {
+
+  var id = prompt("Chose id to delete element");
+
+  id = parseInt(id);
+
+  if (id == ArrayOfID[id-1]) {
+
+    $("#ForBookmarks li:nth-child(" + id + ")").html("");
+
+  }
+
+  else
+
+    alert ("There is no such id plese try again");
+
+}
+
+
+
+function JsonSetUp () {
+
+  loadJSON("json.json", drawData);
+
+}
+
+function drawData (data) {
+
+
+
+}
