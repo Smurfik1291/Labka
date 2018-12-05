893709cfe752a33430ad048493ef928e0b6c0432
Smurfik1291 <45573685+Smurfik1291@users.noreply.github.com>
2018-12-05 12:54:01 +0200

    Add files via upload

---
+++ laba3_jQuery.js
@@ -0,0 +1,86 @@
+alert($("#elem").html());
+
+
+
+
+
+
+
+ $( document ).ready(function() {
+
+
+
+      $('#list-header').css({ 'font-weight': 'bolder' }); 
+
+
+
+      $('#list-header').append("<strong> 1</strong>");
+
+
+
+
+
+	  $('#color').css({ 'color': '#555' });
+
+
+
+      var change = $("#name").html().replace('Peter','Tania');
+
+
+
+	  $("#name").html(change);
+
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+  <script src="laba_3_jQuery.js"></script>
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+</html>
+
+
+
+
+
+
+
+document.getElementsByTagName("ul")[0].style.color = '#555';
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+</html>
